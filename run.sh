b51b2e9a65e65edcac88c40549408275014072b7
den1den <den_1_den@hotmail.com>
2015-12-10 23:03:04 +0100

    Added the actual output of hadoop

--- run.sh
+++ run.sh
@@ -1,7 +1,15 @@
 export JAVA_HOME=/usr/lib/jvm/default-java
 PYTHON_PATH=`which python`
 
+#remove output folder on hadoop
 bin/hdfs dfs -rm -r -f /output
+
+#run hadoop
 bin/hadoop jar streaming.jar -mapper "$PYTHON_PATH mapper.py" -reducer "$PYTHON_PATH reducer.py" -input /input -output /output > output.txt
 
-echo "Done"
+#get results
+bin/hdfs dfs -getmerge /output hadoop-output.txt
+#print results
+head hadoop-output.txt
+
+echo "Script finished"
