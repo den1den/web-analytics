8024b836790ce55fe45eb5a8d763b8747c826657
den1den <den_1_den@hotmail.com>
2015-12-11 14:39:20 +0100

    run.sh: Made jar file relative instead of a symbolic link

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ PYTHON_PATH=`which python`
 bin/hdfs dfs -rm -r -f /output
 
 #run hadoop
-bin/hadoop jar streaming.jar -mapper "$PYTHON_PATH mapper.py" -reducer "$PYTHON_PATH reducer.py" -input /input -output /output > output.txt
+bin/hadoop jar "share/hadoop/tools/lib/hadoop-streaming-2.7.1.jar" -mapper "$PYTHON_PATH mapper.py" -reducer "$PYTHON_PATH reducer.py" -input /input -output /output > output.txt
 
 #get results
 bin/hdfs dfs -getmerge /output hadoop-output.txt
