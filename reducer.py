4e6d12094113dd2e34929f4643bc030356d4ff64
den1den <den_1_den@hotmail.com>
2015-12-16 14:23:17 +0100

    Please do not commit changes that you just copied from previous versions

--- reducer.py
+++ reducer.py
@@ -24,9 +24,9 @@ def count_feel(data):
     for key, group in groupby(data, clasifier):
         try:
             total_count = 0
-            for a, count in group:
+            for pre, post, count in group:
                 total_count += int(count)
-            print(''+a+',feel,'+','+str(total_count))
+            print(''+pre+',feel,'+post+','+str(total_count))
         except ValueError as e:
             print >> sys.stderr, 'Reducer error '+str(e)
 
