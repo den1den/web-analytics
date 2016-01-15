902698354b4c01ea47eb6c39fb4a2443bc2cf299
den1den <den_1_den@hotmail.com>
2016-01-15 13:00:20 +0100

    Cleaned repo

--- MATLAB/ev_edges_uniform.m
+++
@@ -1,8 +0,0 @@
-function [ G ] = ev_edges_uniform( G, seed, remove )
-%EV_EDGES_UNIFORM Randomly only remove only edges
-rng(seed);
-[row,col] = find(G);
-for i = randperm(size(row,1), remove);
-    G(row(i), col(i)) = 0;
-end
-end
