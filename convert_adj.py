ef5790a4d0cdf47d9ab4feb3ec2fa546580b4e61
den1den <den_1_den@hotmail.com>
2016-01-20 22:25:53 +0100

    Added new converter to wrtie edges and nodes to file

--- convert_adj.py
+++ convert_adj.py
@@ -1,12 +1,40 @@
 import sys
 import csv
-with open(sys.argv[1]) as infile:
-    print("Source;Target;Type")
-    for row in csv.reader(infile, delimiter=';'):
+
+mapping_file = "author_labels/2001_labels.txt"
+edges_file = "adjacent/2001_graph.csv"
+edges_output = "adjacent/edges" + ".csv"
+nodes_output = "adjacent/nodes" + ".csv"
+
+mapping = [line.rstrip('\n') for line in open(mapping_file)]
+def get_comm(author_id):
+    return label_mapping[author_id-1]
+
+output = list()
+with open(edges_file) as in_file:
+    output.append("Source;Target;Type")
+    for row in csv.reader(in_file, delimiter=';'):
         i = 0
         src = row[i]
         while i < len(row):
             if row[i] and row[i] != src:
-                print(src + ";" + row[i] + ";" + "Directed")
+                output.append(src + ";" + row[i] + ";Directed" )
             i = i + 1
+
+with open(edges_output, 'w') as out_file:
+    out_file.write('\n'.join(output))
+output = list()
+
+output.append("Id;Community")
+with open(mapping_file) as in_file:
+    output.append("Source;Target;Type")
+    i = 0
+    for row in csv.reader(in_file, delimiter=';'):
+        i = i + 1
+        o = "%s;%s" % (i, row[0])
+        output.append(o)
+
+with open(nodes_output, 'w') as out_file:
+    out_file.write('\n'.join(output))
+output = []
 
