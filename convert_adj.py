cf1bcfd4113776f192132195e3818a166cc8e10c
Dennis van den Brand <dennis.vandenbrand@nubisonline.nl>
2016-01-21 13:41:22 +0100

    added full graph (beware, very large file!)

--- convert_adj.py
+++ convert_adj.py
@@ -4,15 +4,18 @@ import csv
 mapping_file = "author_labels/2001_labels.txt"
 edges_file = "adjacent/2001_graph.csv"
 edges_output = "adjacent/edges" + ".csv"
+edges_n2_output = "adjacent/edges_n2" + ".csv"
 nodes_output = "adjacent/nodes" + ".csv"
 
-inter_weight = 0.01
+inter_weight = 1.0
 intra_weight = 1.0
 
+extra_edges_weight = 0.001
+
+print "started normal"
 mapping = [line.rstrip('\n') for line in open(mapping_file)]
 def get_comm(author_id):
     return mapping[author_id-1]
-
 
 output = list()
 output.append("Id;Community")
@@ -51,3 +54,23 @@ with open(edges_file) as in_file:
 with open(edges_output, 'w') as out_file:
     out_file.write('\n'.join(output))
 
+# Write extra interconnected lines between all nodes of same cummunity (this is a very large file and contains O(n^2) entries)
+print "started n2"
+output = list()
+output.append("Source;Target;Weight")
+output_format = "%s" + ';' + "%s" + ';' + str(extra_edges_weight)
+comms = {'0': list(), '1': list(), '2': list(), '3': list(), '4': list(), '5': list()}
+id = 1
+while id <= len(mapping):
+    comms[mapping[id-1]].append(id)
+    id = id + 1
+for comm_id, commlist in comms.iteritems():
+    for auth_id in commlist:
+        for other_auth in commlist:
+            if auth_id != other_auth:
+                output.append(output_format % (auth_id, other_auth))
+print sum([len(v)*(len(v)-1) for v in comms.itervalues()])
+
+print "Printing lines"
+with open(edges_n2_output, 'w') as out_file:
+    out_file.write('\n'.join(output))
