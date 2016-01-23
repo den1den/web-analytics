22c522c7e9fb14155678b5578077aff0c4b8d684
Dennis van den Brand <dennis.vandenbrand@nubisonline.nl>
2016-01-23 19:39:14 +0100

    changed names of my_type

--- ass_3/c3.py
+++ ass_3/c3.py
@@ -26,7 +26,7 @@ pairs = read_coauther(id_map)
 node_header = ['id', 'my_node_type']
 output = list()
 base_nodes = get_all_gids_from(years_map, base_year)
-my_type = 'bc%s' % base_year
+my_type = 'bc_%s' % base_year
 for gid in base_nodes:
     output.append((
         starting_id_persisten_nodes + gid,
@@ -43,7 +43,7 @@ for gid, years in years_map.items():
             if y in comp_years:
                 output.append((
                     starting_id_non_persitent_nodes + gid,
-                    'new'+str(y)
+                    'new_'+str(y)
                 ))
 #
 # And also add an extra node for each comminuty (only for the presintent nodes)
@@ -51,7 +51,7 @@ for gid, years in years_map.items():
 for comm in classes:
     output.append((
         starting_id_extra + comm,
-        'comm' + str(comm)
+        'comm_' + str(comm)
     ))
 write(output, node_header, out_nodes_filename)
 
@@ -69,7 +69,7 @@ def get_node_id(gid):
 output = list()
 weight = weight_normal
 edge_id = 1
-type = "bc" + str(base_year)
+my_type = "bc_" + str(base_year)
 base_edges = list()
 for pair_str, years in pairs.items():
     if base_year in years:
@@ -77,7 +77,7 @@ for pair_str, years in pairs.items():
         a, b = string_to_pair(pair_str)
         src = get_node_id(int(a))
         dst = get_node_id(int(b))
-        output.append((src, dst, "Directed", starting_id_persisten_nodes + edge_id, type, weight))
+        output.append((src, dst, "Directed", starting_id_persisten_nodes + edge_id, my_type, weight))
         base_edges.append(pair_str)
         edge_id += 1
 assert edge_id < starting_id_persisten_nodes
@@ -86,19 +86,19 @@ assert edge_id < starting_id_persisten_nodes
 #
 weight = weight_commcenter
 edge_id = 1
-type_format = "bccomm%s"
+type_format = "comm_%s"
 for gid, comm in classification_mapping[base_year].items():
-    type = type_format % comm
+    my_type = type_format % comm
     dst = starting_id_extra + comm
     src = starting_id_persisten_nodes + gid
-    output.append((src, dst, "Directed", starting_id_extra + edge_id, type, weight))
+    output.append((src, dst, "Directed", starting_id_extra + edge_id, my_type, weight))
     edge_id += 1
 #
 # Also output edges of the new nodes
 #
 weight = weight_normal
 edge_id = 1
-type_format = "new%s"
+type_format = "new_%s"
 for pair_str, years in pairs.items():
     if pair_str not in base_edges:
         a, b = string_to_pair(pair_str)
@@ -108,8 +108,8 @@ for pair_str, years in pairs.items():
             if y in comp_years:
                 src = get_node_id(gida)
                 dst = get_node_id(gidb)
-                type = type_format % y
-                output.append((src, dst, "Directed", starting_id_non_persitent_nodes + edge_id, type, weight))
+                my_type = type_format % y
+                output.append((src, dst, "Directed", starting_id_non_persitent_nodes + edge_id, my_type, weight))
                 edge_id += 1
 write(output, ['Source', 'Target', 'Type', 'id', 'my_edge_type', 'weight'], out_edges_filename)
 print("completed in %.3f seconds" % (time.time() - start_time))
