277a1765641d4d67319e0175c6ba311c7921adc3
Dennis van den Brand <dennis.vandenbrand@nubisonline.nl>
2016-01-23 02:29:56 +0100

    Input for gephi completed

--- ass_c/c.py
+++ ass_c/c.py
@@ -1,153 +1,18 @@
-import sys
-import csv
+import time
+from settings import nodes_header, nodes_format, out_nodes_filename, weight_commcenter, weight_normal, years, \
+    set_name_original, set_name_extra, classes, edges_header, edges_format, out_edges_filename
 
-years = range(2001, 2010)
-gt = "text" # graph or text
-
-weight_normal = 1.0
-weight_commcenter = 0.01
-
-out_nodes_filename = "nodes.csv"
-out_edges_filename = "edges.csv"
-
-edges_header = "Source;Target;Type;Timestamp;Set;Weight"
-# Source: source node id
-# Target: target node id
-# Type: directed/undirected
-# Timestamp: array of years this edge is in
-# Set: 10=author edge, 2x=community x edge
-# Weight: the weight of this edge
-edges_format = "%s;%s;%s;%s;%s;%s" #5
-
-nodes_header = "Id;Timestamp;Set"
-# Id: the global id
-# Timestamp: array of years this node is in
-# Set: 10=author node, 2x=community x node
-nodes_format = "%s;%s;%s"
-
-#
-# Create global id mappings
-#
-in_filename = "../author_mapping/%s_mapping.txt"
-name_id_mapping = dict()  # name -> global_id
-year_id_mapping = {y: dict() for y in years} # year,id -> global_id
-id_years_mapping = dict()  # global_id -> year
-for year in years:
-    with open(in_filename % year) as in_file:
-        for row in csv.reader(in_file, delimiter=','):
-            year_id = int(row[0])
-            try:
-                name = row[1].strip()
-            except:
-                print row
-                exit()
-            
-            if name in name_id_mapping:
-                global_id = name_id_mapping[name]
-                id_years_mapping[global_id].append(year)
-            else:
-                global_id = len(name_id_mapping)+1
-                name_id_mapping[name] = global_id
-                id_years_mapping[global_id] = [year]
-            
-            year_id_mapping[year][year_id] = global_id
-            
-
-#
-# Load baseline group mapping
-#
-in_filename = "../%s_pred_label_"+gt+".txt"
-year_baseline_mapping = {y: dict() for y in years} # year,global_id -> baseline_comm
-for year in years:
-    with open(in_filename % year) as in_file:
-        year_id = 1
-        for row in csv.reader(in_file, delimiter=','):
-            global_id = year_id_mapping[year][year_id]
-            baseline_comm = int(row[0])
-            year_baseline_mapping[year][global_id] = baseline_comm
-            year_id += 1
-
-#
-# Load our group mapping
-#
-in_filename = "../%s_pred_label_"+gt+".txt"
-year_pred_mapping = {y: dict() for y in years} # year,global_id -> pred_comm
-for year in years:
-    with open(in_filename % year) as in_file:
-        year_id = 1
-        for row in csv.reader(in_file, delimiter=','):
-            global_id = year_id_mapping[year][year_id]
-            pred_comm = int(row[0])
-            year_pred_mapping[year][global_id] = pred_comm
-            year_id += 1
-
-#
-# Check group simularity (Maps our mapping to the most similar baseline mapping)
-#
-year_comm_mapping = {y: [] for y in years} # year, pred_comm -> baseline_comm
-year_purity = {y: [] for y in years} # year -> purity
-import itertools
-max_similarity = 0.0
-for year in years:
-    max_matches = 0
-    total_authors = len(year_id_mapping[year])
-    for c in itertools.combinations(range(0, 5), 6): #brute force 6! = 720
-        matches = 0
-        for year_id, global_id in year_id_mapping[year].iteritems():
-            baseline_comm = year_baseline_mapping[year][global_id]
-            pred_comm = year_baseline_mapping[year][global_id]
-            matches += c[pred_comm] == baseline_comm
-        if matches > max_matches:
-            max_matches = matches
-            year_comm_mapping[year] = c
-    year_purity[year] = max_matches / total_authors
-# And replace our mapping with the correct one
-year_pred_mapping = {y: dict() for y in years}
-for y in years:
-    for g_id, pred_com in year_pred_mapping[y].iteritems():
-        year_pred_mapping[y][g_id] = year_comm_mapping[y][pred_com]
-
-#
-# Merge the adjacent filesets
-#
-in_filename = "../adjacent/%s_graph.adjlist"
-def pair_to_string(gida, gidb):
-    return str(gida)+"|"+str(gidb)
-def string_to_pair(string):
-    return string.split("|")
-pair_to_years = dict()
-for year in years:
-    with open(in_filename % year) as in_file:
-        for row in csv.reader(in_file, delimiter=' '):
-            src = int(row[0])
-            i = 1
-            while(row[i]):
-                dst = int(row[i])
-                
-                if src < dst:
-                    a = src
-                    b = dst
-                else:
-                    a = dst
-                    b = src
-                
-                #found a pair
-                pair_str = pair_to_string(a, b)
-                
-                if pair_str in pair_to_years:
-                    pair_to_years[pair_str].append(year)
-                else:
-                    pair_to_years[pair_str] = [year]
-                
-                i += 1
+from read import read_global_mapping, read_clasification_mapping, read_coauther, write, string_to_pair
+start_time = time.time()
 
 
-# output function
-def write(arr_lst, header, format, to_file):
-    with open(to_file, 'w') as out_file:
-        out_file.write(header+'\n')
-        for arr in arr_list:
-            out_file.write((arr % format)+'\n')
+id_map, years_map = read_global_mapping()
+
+
+year_classification_mapping, year_purity = read_clasification_mapping(id_map)
+
+
+pairs = read_coauther(id_map)
 
 
 #
@@ -155,16 +20,20 @@ def write(arr_lst, header, format, to_file):
 #
 output = list()
 # first output the 6 (hidden) community nodes
-for comm in range(0,5):
-    output.append([666000+comm, years, 20+comm])
+starting_id = 66666666
+for comm in range(0, 5):
+    id = starting_id + comm
+    timestamp = years
+    set = set_name_extra % comm
+    output.append((id, timestamp, set, ))
 # Then output all the global id's
-for global_id in id_year_mapping:
-    # get all the years this node was in
-    years = id_years_mapping[global_id]
+for global_id, years in years_map.items(): # get all the years this node was in
     # get all the communities this node was in per year
-    comms = [10+year_pred_mapping[y][global_id] for y in years] # normalize to baseline communities
-    output.append([666000+comm, years, comms])
-write(output, nodes_header, node_format, node_file)
+    id = global_id
+    timestamp = years
+    set = [set_name_original % year_classification_mapping[y][global_id] for y in years]
+    output.append((id, timestamp, set, ))
+write(output, nodes_header, nodes_format, out_nodes_filename)
 
 
 #
@@ -175,23 +44,29 @@ output = list()
 # output an seperate edge for each year (resulting in approx 30000 edges)
 # if this is to slow we could add the timestamp for each edge to get only 6000 edges (one per author)
 weight = weight_commcenter
-for g_id, years in id_years_mapping.iteritems():
-    dst = g_id
-    
-    this_auth_comm_years = {c: dict() for c in range(0,5)}
+for global_id, years in years_map.items():
+    dst = global_id
+
+    # collect all the communitie sfor each year
+    years_per_comm = {c: list() for c in classes}
     for y in years:
-        # add the year to the correct comunity
-        comm_for_this_year = year_pred_mapping[y][g_id]
-        this_auth_comm_years[comm_for_this_year].append(y)
+        comm_for_this_year = year_classification_mapping[y][global_id]
+        years_per_comm[comm_for_this_year].append(y)
     
-    for comm, years in this_auth_comm_years.itertools():
-        src = 666000+comm
-        Set = 20+comm
-        timestamp = years
-        output.append((src, dst, timestamp, Set, weight))
+    for comm, years in years_per_comm.items():
+        if len(years) > 0:
+            src = starting_id + comm # from the hidden comunity node
+            set = set_name_extra % comm
+            timestamp = years
+            output.append((src, dst, "Directed", timestamp, set, weight))
 # Then output all the normal nodes
 weight = weight_normal
-for pair_str, years in pair_to_years.iteritems():
+for pair_str, years in pairs.items():
     a, b = string_to_pair(pair_str)
-    output.append((a, b, years, 10, weight))
-write(output, nodes_header, node_format, node_file)
+    src = a
+    dst = b
+    timestamp = years
+    set = set_name_original % 0
+    output.append((a, b, "Undirected", timestamp, set, weight))
+write(output, edges_header, edges_format, out_edges_filename)
+print("completed in %.3f seconds" % (time.time() - start_time))
---
+++ ass_c/settings.py
@@ -0,0 +1,55 @@
+import itertools
+
+years = range(2001, 2010)
+gt = "text" # graph or text
+
+weight_normal = 1.0
+weight_commcenter = 0.01
+
+out_nodes_filename = "nodes.csv"
+out_edges_filename = "edges.csv"
+
+set_name_original = "1%s"
+set_name_extra = "2%s"
+
+edges_header = "Source;Target;Type;Timestamp;Set;Weight"
+# Source: source node id
+# Target: target node id
+# Type: directed/undirected
+# Timestamp: array of years this edge is in
+# Set: 10=author edge, 2x=community x edge
+# Weight: the weight of this edge
+edges_format = "%s;%s;%s;%s;%s;%s"  # 6
+
+nodes_header = "Id;Timestamp;Set"
+# Id: the global id
+# Timestamp: array of years this node is in
+# Set: 10=author node, 2x=community x node
+nodes_format = "%s;%s;%s"
+
+
+filenames = {
+    'author_mapping': '../author_mapping/%s_mapping.txt',
+    'prediction_labels': '../%s_pred_label_' + gt + '.txt',
+    'baseline_labels': '../author_labels/%s_labels.txt',
+    'coauthor': '../adjacent/%s_graph.adjlist',
+}
+
+classes = [0, 1, 2, 3, 4, 5]
+
+purity_classification_group_mapping = {
+    y: itertools.permutations(classes, 6) for y in years
+}
+
+purity_classification_group_mapping_ = {
+    2001: [(1, 2, 0, 4, 5, 3)],
+    2002: [(2, 4, 3, 1, 0, 5)],
+    2003: [(1, 2, 5, 4, 3, 0)],
+    2004: [(4, 3, 0, 2, 5, 1)],
+    2005: [(1, 2, 3, 4, 0, 5)],
+    2006: [(1, 5, 2, 0, 4, 3)],
+    2007: [(4, 3, 0, 5, 2, 1)],
+    2008: [(0, 2, 1, 3, 5, 4)],
+    2009: [(1, 4, 0, 2, 5, 3)],
+}
+
