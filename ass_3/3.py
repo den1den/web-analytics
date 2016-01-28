2ad56aa310bebccbcace9c0e73fa8534efe06f19
Dennis van den Brand <dennis.vandenbrand@nubisonline.nl>
2016-01-28 20:48:49 +0100

    Fix switch of collums. AND added a difference between 'newcomers' and 'returnees'. Design comment: We did this because the number of newcomers was very large. Splitting it up made more clear/informative

--- ass_3/3.py
+++ ass_3/3.py
@@ -1,5 +1,7 @@
 import time
 import settings
+from settings import all_years, classes
+
 settings.print_purity = False
 from read import read_global_mapping, read_clasification_mapping
 all_years_min1 = range(2001, 2010)
@@ -13,7 +15,13 @@ id_map, years_map = read_global_mapping()
 # classification_mapping[year][global_id] = community
 classification_mapping, year_purity = read_clasification_mapping(id_map)
 
+totals = {
+    y: {COMM: len([ids for ids, comm in classification_mapping[y].items() if comm == COMM]) for COMM in classes}
+    for y in all_years
+}
+
 table = {y: {} for y in all_years_min1}
+n_statatisitcs = 6
 
 for comm in settings.classes:
     for year_a in all_years_min1:  # 2001 - 2009
@@ -23,43 +31,48 @@ for comm in settings.classes:
         stayers = 0         # comm -> comm
         switched_out = 0    # comm -> other comm
         switched_in = 0     # other comm -> comm
-        new_commers = 0     # no listing -> comm
+        returnee = 0        # no listing previous year -> comm
+        new_commers = 0     # no listing any prev year -> comm
 
-        authors_in_year_a = list()
+        for author, old_comm in classification_mapping[year_a].items():
+            if old_comm == comm:
+                # This author is from the previous year and this previous community
 
-        for _dummy, author in id_map[year_a].items():
-            authors_in_year_a.append(author)
-
-            if not author in classification_mapping[year_b]:
-                quitters += 1
-            else:
-                # it is also in a community the next year
-                next_comm =  classification_mapping[year_b][author]
-
-                # comm is the community in the previous year
-                if next_comm == comm:
-                    # this author has been in the same community
-                    stayers += 1
+                if not author in classification_mapping[year_b]:
+                    quitters += 1
                 else:
-                    # this auther has switched from comm to curr_comm
-                    switched_out += 1
+                    # it is also in a community the next year
+                    next_comm =  classification_mapping[year_b][author]
 
-        for _dummy, author in id_map[year_b].items():
-            # for each author in the next year
-            if author not in authors_in_year_a:
-                # This is a new author
-                new_commers += 1
-            else:
-                # The auther was already in year a
-                prev_comm = classification_mapping[year_a][author]
-                if prev_comm == comm:
-                    # This author has been in the same community (and is already counted)
-                    pass
-                else:
-                    # this author has switched from prev_comm to comm
+                    # comm is the community in the previous year
+                    if next_comm == comm:
+                        # this author has been in the same community
+                        stayers += 1
+                    else:
+                        # this auther has switched from comm to curr_comm
+                        switched_out += 1
+
+        for author, new_comm in classification_mapping[year_b].items():
+            # This author is from the next year and has a community
+            if new_comm == comm:
+                # it has the same community now
+
+                if author not in classification_mapping[year_a]:
+                    # did not exist in previous year
+                    first_apperance_year = min(years_map[author])
+                    if first_apperance_year < year_a:
+                        returnee += 1
+                    else:
+                        new_commers += 1
+                elif classification_mapping[year_a][author] != comm:
+                    # the author had a different community last year
                     switched_in += 1
+                else:
+                    #this author was in the same community last year so
+                    pass
 
-        table[year_a][comm] = (quitters, stayers, switched_out, switched_in, new_commers, )
+        table[year_a][comm] = (quitters, stayers, switched_out, switched_in, new_commers, returnee)
+        assert len(table[year_a][comm]) == n_statatisitcs
 
 
 output = open('3/3_output.html', 'w')
@@ -74,19 +87,29 @@ for y in all_years_min1:
 print('</tr></thead>', file=output)
 
 print('<tbody>', file=output)
-for row_i in range(5 * len(settings.classes)):
+for row_i in range(n_statatisitcs * len(settings.classes)):
     print('<tr>', file=output)
-    comm = int(row_i / 5)
+    comm = int(row_i / n_statatisitcs)
     print('<td>%s</td>' % comm, file=output)
-    val = row_i % 5
-    print('<td>%s</td>' % ['Quit', 'Stay', 'Switch In', 'Switch Out', 'New'][val], file=output)
+    val = row_i % n_statatisitcs
+    print('<td>%s</td>' % ['Quit', 'Stay', 'Switch Out', 'Switch In', 'New', 'Returnee'][val], file=output)
     for y in all_years_min1:
         print('<td>%s</td>' % table[y][comm][val], file=output)
     print('</tr>', file=output)
 print('</tbody></table>', file=output)
 
-print("<p>Quit: comm -> no listing<br/>Stay: comm -> comm<br/>Switch In: other comm -> comm<br/>"
-      "Switch Out: comm -> other comm<br/>New: no listing -> comm</p>", file=output)
+print("<p>"
+      "Quit: comm -> no listing<br/>"
+      "Stay: comm -> comm<br/>"
+      "Switch In: other comm -> comm<br/>"
+      "Switch Out: comm -> other comm<br/>"
+      "New: no listing in any prev year -> comm<br/>"
+      "Returnee: a listing in some prev year (for some community) -> comm"
+      "</p>", file=output)
+
+print("<h2>Totals</h2>", file=output)
+#print("<pre>%s</pre>" % str(totals).replace(',', '\n').replace(':', '\n').replace('{','').replace('}',''), file=output)
+print("<pre>%s</pre>" % str(totals).replace(', ', ', \n'), file=output)
 
 print("Output is written to "+str(output.name))
 print("completed in %.3f seconds" % (time.time() - start_time))
